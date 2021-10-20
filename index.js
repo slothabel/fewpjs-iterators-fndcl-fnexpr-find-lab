008c13ab0048fa905984ab27b5bdf34eead64ebf
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-20 13:06:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 
-// Write a function called superbowlWin() in index.js that:
-//  * Receives 1 argument, an Array of JavaScript Objects 
-// * Each object has two properties: year and result
-//  * Use find() to test each Object to see if the result is "W" — a win!
-//  *  * superbowlWin() should return the year when the win occurred (if it occurred at all!). 
-// If no win is found, it should return, sadly, undefined
-
-// roommates.find( function(s) { return s === "winston" })
-//  result === "W"
 function superbowlWin(obj){
-    const wonGame = obj.find(({result}) => console.log(result))
+    //with the {} around 'result' this isolates the result attribute inside of the object being
+    //called in the .find iteration
+    const wonGame = obj.find(({result}) => result === "W")
+    // now, here, without the {} it makes 'result' a simple param, returning the entire object
+    
     // const wonGame = obj.find((result) => console.log(result))
-    //  console.log(wonGame['year'])
+    //the lab wanted superbowlWin to return undefined if the record has no win objects, so 
+    // this if/else is saying "if wonGame is true, return the year. else return undefined"
     if(wonGame){
         return wonGame['year']
     }
