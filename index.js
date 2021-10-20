e053c3b1cba3a5a284ff4f07ffed635c1380c2f4
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-20 12:45:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+// Write a function called superbowlWin() in index.js that:
+//  * Receives 1 argument, an Array of JavaScript Objects 
+// * Each object has two properties: year and result
+//  * Use find() to test each Object to see if the result is "W" — a win!
+//  *  * superbowlWin() should return the year when the win occurred (if it occurred at all!). 
+// If no win is found, it should return, sadly, undefined
+
+// roommates.find( function(s) { return s === "winston" })
+
+function superbowlWin(obj){
+    const wonGame = obj.find(({result}) => result === "W")
+     console.log(wonGame)
+    // return wonGame
+}
